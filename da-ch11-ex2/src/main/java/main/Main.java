99aa9fde84873828e0c2d4bcfa31b7019fe0a973
bion0625 <bion0625@gmail.com>
2024-10-14 15:36:48 +0900

    11.2 힙 덤프를 수집하여 메모리 누수가 발생하는 곳 찾기

---
+++ da-ch11-ex2/src/main/java/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+// VMoptions -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heapdump.bin -Xmx100m
+public class Main {
+
+  private static List<Product> products = new ArrayList<>();
+
+  public static void main(String[] args) {
+    Random r = new Random();
+    while (true) {
+      Product p =new Product();
+      p.setName("Product " + r.nextInt());
+      products.add(p);
+    }
+  }
+}
